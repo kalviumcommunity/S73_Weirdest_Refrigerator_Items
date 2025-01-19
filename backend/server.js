ce46bd1fec5b09112326c46189ebe58496ec676f
atharv1205 <atharvadudhe125@gmail.com>
2025-01-19 10:09:26 +0530

    updated the root get request

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,15 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT;
 
-
 app.get('/ping', (req, res) => {
     res.json({
         message: "Pong",
         status: "success",
     });
+});
+
+app.get('/', (req, res) => {
+    res.send("<h1>Head to /ping Page!</h1>")
 });
 
 app.listen(PORT, () => {
